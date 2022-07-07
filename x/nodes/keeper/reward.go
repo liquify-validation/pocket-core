2591654fe90035bc8bb249e8ef3ffd6ef1cdb3b5
Andrew Pohl <andypohl@hotmail.co.uk>
2022-07-07 13:04:04 +0100

    fix typo

--- x/nodes/keeper/reward.go
+++ x/nodes/keeper/reward.go
@@ -31,7 +31,7 @@ func (k Keeper) RewardForRelays(ctx sdk.Ctx, relays sdk.BigInt, address sdk.Addr
 
 		stake := validator.GetTokens()
 		//floorstake to the lowest bin multiple or take ceiling, whicherver is smaller
-		flooredStake := sdk.MinInt(stake.Sub(stake.Mod(k.ServicerStakeFloorMultiplier(ctx))),(k.ServicerStakeWeightCeiling(ctx).Sub(stake.Mod(k.ServicerStakeFloorMultiplier(ctx)))))
+		flooredStake := sdk.MinInt(stake.Sub(stake.Mod(k.ServicerStakeFloorMultiplier(ctx))),(k.ServicerStakeWeightCeiling(ctx).Sub(k.ServicerStakeWeightCeiling(ctx).Mod(k.ServicerStakeFloorMultiplier(ctx)))))
 		//Convert from tokens to a BIN number
 		Bin := flooredStake.Quo(k.ServicerStakeFloorMultiplier(ctx))
 		//calculate the weight value, weight will be a floatng point number so cast to DEC here and then truncate back to big int
